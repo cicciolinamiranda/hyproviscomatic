605af63f48983d60ea19d6ec31d94dc079f685d7
Cicciolina Miranda <cicciolina@cicciolina-macbookpro.roam.corp.google.com>
2016-12-06 13:39:09 +0800

    Fix git ignored files

--- MyApplication/app/src/main/java/com/uprise/ordering/rest/listeners/RestAsyncTaskListener.java
+++
@@ -1,9 +0,0 @@
-package com.uprise.ordering.rest.listeners;
-
-/**
- * Created by cicciolina on 10/16/16.
- */
-public interface RestAsyncTaskListener {
-    void doInBackground();
-    void result();
-}
